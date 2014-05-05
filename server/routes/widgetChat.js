a0a968cb10292061e18d7b76117d37c70892a2f0
Junior Oliveira <junolive@gmail.com>
2014-05-05 00:56:00 -0300

    Bug no acesso do chat via widget

--- server/routes/widgetChat.js
+++ server/routes/widgetChat.js
@@ -1,7 +1,7 @@
 ﻿var passport = require('passport');
 
 exports.get = function (req, res) {
-    res.render('chat', { relativePath: 'http://widget-chat.herokuapp.com' });
+    res.render('chat', { relativePath: 'http://widget-chat.herokuapp.com:80' });
 };
 
 exports.post = passport.authenticate('local-login', {
